ba9e36067e919bfd66a961e42c45186d33dff904
Jan Gnezda <jan@duuoo.io>
2018-09-21 01:16:17 +0200

    Simplify ErrorMessage component

--- packages/web/src/components/ErrorMessage/ErrorMessage.jsx
+++ packages/web/src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,32 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import { Button, Label } from '../FormElements';
 import Title from '../Title';
 
-class ErrorMessage extends React.Component {
-  constructor(props) {
-    super(props);
+const ErrorMessage = ({ onBack }) => (
+  <React.Fragment>
+    <Title>Error occured</Title>
 
-    this.state = {
-    };
-  }
+    <Label>The account number or pin is invalid.</Label>
 
-  render() {
-    return (
-      <React.Fragment>
-        <Title>Error occured</Title>
-
-        <Label>The account number or pin is invalid.</Label>
-
-        <Button
-          onClick={this.props.onBack}
-        >
-          Back
-        </Button>
-      </React.Fragment>
-    );
-  }
-};
+    <Button
+      onClick={onBack}
+    >
+      Back
+    </Button>
+  </React.Fragment>
+);
 
 export default ErrorMessage;
